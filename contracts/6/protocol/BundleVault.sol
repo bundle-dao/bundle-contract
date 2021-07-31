9a70302f601e2fa020b66e219b19fbeff7c3be7b
Zenith <0xazimuth@protonmail.com>
2021-07-30 18:13:32 -0700

    Progress on withdraw, conversions

--- contracts/6/protocol/BundleVault.sol
+++ contracts/6/protocol/BundleVault.sol
@@ -3,12 +3,14 @@ pragma solidity 0.6.12;
 
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/math/Math.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./interfaces/IController.sol";
 
 contract BundleVault is Ownable {
     using SafeMath for uint256;
+    using Math for uint256;
     using SafeERC20 for IERC20;
 
     event LogShareChanged(
@@ -104,6 +106,7 @@ contract BundleVault is Ownable {
     function deposit(uint256 amount) external {
         // Merge user deposits
         _mergeDeposits(msg.sender);
+        _mergeCumulativeDeposits();
 
         // Load relevant data
         uint256 time = block.timestamp.mod(1 days).mul(1 days);
@@ -150,12 +153,48 @@ contract BundleVault is Ownable {
     }
 
     function withdraw(uint256 amount) external {
+        // Merge user deposits
+        _mergeDeposits(msg.sender);
+        _mergeCumulativeDeposits();
 
+        // Load relevant data
+        User storage user = _users[msg.sender];
+        uint256 mutableAmount = amount;
+
+        // Subtract from existing deposits first
+        for (uint256 i = 0; i < user.deposits.length; i++) {
+            uint256 subAmount = user.deposits[user.deposits.length - 1 - i].balance.min(mutableAmount);
+            user.deposits[user.deposits.length - 1 - i].balance = user.deposits[user.deposits.length - 1 - i].balance.sub(subAmount);
+            mutableAmount = mutableAmount.sub(subAmount);
+
+            // Update cumulative deposit
+            uint256 time = user.deposits[user.deposits.length - 1 - i].time;
+            for (uint256 j = 0; i < _cumulativeDeposits.length; i++) {
+                if (_cumulativeDeposits[j].time == time) {
+                    _cumulativeDeposits[j].balance = _cumulativeDeposits[j].balance.sub(subAmount);
+                }
+            }
+
+            if (mutableAmount == 0) {
+                break;
+            }
+        }
+
+        // Subtract from active balance
+        uint256 activeAmount = mutableAmount == 0 ? 0 : _convertToActive(mutableAmount);
+
+        require(user.activeBalance >= activeAmount, "ERR_AMOUNT_TOO_LARGE");
+
+        user.activeBalance = user.activeBalance.sub(activeAmount);
+        _cumulativeBalance = _cumulativeBalance.sub(activeAmount);
+        
+        _bdl.transfer(msg.sender, amount);
     }
 
     /* ========== Fee Collection ========== */
 
     function collect() external {
+        _mergeCumulativeDeposits();
 
     }
 
@@ -170,8 +209,7 @@ contract BundleVault is Ownable {
         for (uint256 i = 0; i < user.deposits.length; i++) {
             if (user.deposits[i].time <= time) {
                 uint256 balance = user.deposits[i].balance;
-                user.activeBalance = user.activeBalance.add(balance);
-                _cumulativeBalance = _cumulativeBalance.add(balance);
+                // FIX user.activeBalance = user.activeBalance.add(balance);
                 user.deposits[i].balance = 0;
                 mergeCounter++;
             }
@@ -184,5 +222,49 @@ contract BundleVault is Ownable {
         for (uint256 i = 0; i < mergeCounter; i++) {
             user.deposits.pop();
         }
+    }
+
+    function _mergeCumulativeDeposits() internal {
+        uint256 time = block.timestamp.sub(DELAY);
+        uint256 mergeCounter = 0;
+
+        // Merge deposit if older than 7 days
+        for (uint256 i = 0; i < _cumulativeDeposits.length; i++) {
+            if (_cumulativeDeposits[i].time <= time) {
+                uint256 balance = _cumulativeDeposits[i].balance;
+                // FIX _cumulativeBalance = _cumulativeBalance.add(balance);
+                _cumulativeDeposits[i].balance = 0;
+                mergeCounter++;
+            }
+        }
+
+        for (uint256 i = 0; i < _cumulativeDeposits.length.sub(mergeCounter); i++) {
+            _cumulativeDeposits[i] = _cumulativeDeposits[i + mergeCounter];
+        }
+
+        for (uint256 i = 0; i < mergeCounter; i++) {
+            _cumulativeDeposits.pop();
+        }
+    }
+
+    function _convertToActive(uint256 amount) 
+        internal view 
+        returns(uint256) 
+    {
+        // Should discount BDL from pending deposits
+        return amount.mul(_bdl.balanceOf(address(this)).sub(_getPendingBalance())).div(_cumulativeBalance);
+    }
+
+    function _getPendingBalance()
+        internal view
+        returns (uint256)
+    {
+        uint256 pendingBalance = 0;
+
+        for (uint256 i = 0; i < _cumulativeDeposits.length; i++) {
+            pendingBalance += _cumulativeDeposits[i].balance;
+        }
+
+        return pendingBalance;
     }
 }
